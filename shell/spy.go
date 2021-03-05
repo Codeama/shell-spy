21ec90b54d24318ae571eb22ce97cdb13aaebecd
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2021-03-05 09:04:08 +0000

    refactor and use table-driven test data

--- shell/spy.go
+++ shell/spy.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+// Parser represents a function that parses string text
+// and returns a tring and slice of strings
+type Parser func(string) (string, []string)
+
 // ParseCommand accepts a string and returns the command name and args
 func ParseCommand(cmd string) (string, []string) {
 	stringResult := strings.Split(cmd, " ")
@@ -15,8 +19,8 @@ func ParseCommand(cmd string) (string, []string) {
 }
 
 // Execute accepts a command argument and writes the result to a writer
-func Execute(writer io.Writer, parser func(string) (string, []string), commandLine string) {
-	name, args := parser(commandLine)
+func Execute(writer io.Writer, parserFunc Parser, commandLine string) {
+	name, args := parserFunc(commandLine)
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = writer
 	err := cmd.Run()
--- shell/spy_test.go
+++ shell/spy_test.go
@@ -5,33 +5,29 @@ import (
 	"testing"
 )
 
-// func TestExecute(t *testing.T) {
-// 	var out bytes.Buffer
-// 	want := "spy.go\nspy_test.go\n"
-// 	Execute(&out, "ls")
-// 	got := fmt.Sprint(&out)
-// 	if got != want {
-// 		t.Errorf("Expected: %s, got: %s", want, got)
-// 	}
-// }
-
 func TestParseCommand(t *testing.T) {
-	testData := struct {
+	testData := []struct {
 		cmd  string
 		name string
 		args []string
 	}{
-		"echo Bukola",
-		"echo",
-		[]string{"Bukola"},
+		{"echo Bukola", "echo", []string{"Bukola"}},
+		{"test it works fine", "test", []string{"it", "works", "fine"}},
+		{"cat /file-path", "cat", []string{"/file-path"}},
+		{"cd ..", "cd", []string{".."}},
+		{"echo", "echo", []string{}},
 	}
-	name, args := ParseCommand(testData.cmd)
 
-	if testData.name != name {
-		t.Errorf("Expected command name: %s, got: %s", testData.name, name)
-	}
-	if !reflect.DeepEqual(testData.args, args) {
-		t.Errorf("Expected args: %s, got %s", testData.args, args)
+	for _, td := range testData {
+
+		name, args := ParseCommand(td.cmd)
+
+		if td.name != name {
+			t.Errorf("Expected command name: %s, got: %s", td.name, name)
+		}
+		if !reflect.DeepEqual(td.args, args) {
+			t.Errorf("Expected args: %s, got %s", td.args, args)
+		}
 	}
 
 }
